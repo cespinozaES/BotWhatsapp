4cc3fbbee87ced97a0afe46afbfc762e46a2d632
DESKTOP-F5QNF2S\OFIMATICA-22 <cespinoza@electrosoftware.net>
2022-05-19 09:00:24 -0500

    Creacion y conexion QR, envio de mensajes y respuestas mediante chat a un numero de telefono especifico

--- app.js
+++ app.js
@@ -1,33 +1,60 @@
-const fs = (require('fs'));
-const { Client } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
-const SESSION_FILE_PATH = './session.json';
-let client; 
-let sessionData;
-const sesionIniciada = () => {
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const client = new Client({
+    authStrategy: new LocalAuth()
+});
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log('Se ha establecido la conexión');
+    escucharMensaje();
+});
+
+
+const escucharMensaje = () => {
+    client.on('message', message => {
+        console.log(message.from);
+        if('573013358475@c.us' === message.from){
+            console.log(`Número correcto, mensaje: ${message.body}`);
+            respuestas(message);
+            return;
+        }else{
+            console.log(`Número Incorrecto, mensaje: ${message.body}`);
+            return;
+        }
+    })
+}
+
+const respuestas = (message) => {
+    client.sendMessage(message.from, 'Bienvenido a MovilTech S.A.S para solicitar alguno de nuestros servicios elige la opción que necesitas'+
+    '\n'+'\n1. Soporte Técnico \n2. Ventas \n3. PQRS');
+    switch (message.body){
+        case  '1':
+            client.sendMessage(message.from, 'Para soporte tecnico disponemos de las siguientes opciones '+
+            '\n1.Soporte de Moviles \n2. Soporte de PC \n3. Soporte de otros dispositivos');
+            break;
+        case '2':
+            client.sendMessage(message.from, 'Para ventas disponemos de las siguientes opciones '+
+            '\n1.Ventas de Móviles \n2. Ventas de Pc \n3. Ventas de Audífonos \n4 Ventas de otros dispositivos');
+            break;
+        case '3':
+            client.sendMessage(message.from, 'Para PQRS disponemos de las siguientes opciones'+
+            '\n1. Peticiones \n2. Quejas \n3. Reclamos \n4. Estado de la Peticiòn/Queja o Reclamo');
+            break;
+        //default:
+         //   client.sendMessage(message.from, 'La opcion digitada no existe, por favor digite el numero de una de las opciones');
+
+    }
+    //if(message.body === '!ping') {
+       // message.reply('pong');
+     //   console.log('Respondiste Pong');
+   // }
 }
 
 
-const sinSesion = () => {
-    console.log('Aun no se ha iniciado sesión');
-    client = new Client();
-    client.on('qr', (qr) => {
-        console.log('QR RECEIVED', qr);
-        qrcode.generate(qr, { small: true });
-    });
 
-    client.on('authenticated', (session) => {
-        //Guardamos datos de la sesion
-        sessionData = session;
-        fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), (error) => {
-            if (error) {
-                console.log(error);
-            }
-        });    
-    });
-    client.initialize();
 
-}
-
-//Esto se encarga de revisar si existe ese archivo para ejecutar una funcion u otra 
-(fs.existsSync(SESSION_FILE_PATH)) ? sesionIniciada() : sinSesion();
+client.initialize();
